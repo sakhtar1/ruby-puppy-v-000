55595ef0537bad51fd91d9020c694f55878be4a5
Sharmina Akhtar <sakht318@gmail.com>
2017-11-22 09:20:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
