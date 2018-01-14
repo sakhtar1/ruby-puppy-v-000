6ebf7d10764007e73eb99d92b52c724c250a35fb
Sharmina Akhtar <sakht318@gmail.com>
2018-01-14 17:09:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,15 @@ class Dog
     @name = name
     @@all << self
   end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
   
 end
